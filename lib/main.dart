2a2f1f6b9560ea828ef919e3b6ccd283f76f00d1
Elioephrem <151877315+Elioephrem@users.noreply.github.com>
2024-01-01 21:46:13 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,1 @@
-import 'package:flutter/material.dart';
-import 'login_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginScreen(''),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
